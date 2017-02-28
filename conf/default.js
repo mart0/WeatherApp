065f72e82ed98a1d7778da4f322730e4eece666f
Martin Marinov <marinov9210@gmail.com>
2017-02-28 17:42:49 +0200

    Remove mysql connector

--- conf/default.js
+++ conf/default.js
@@ -88,15 +88,14 @@ module.exports = {
 
 	// your connector configuration goes here
 	connectors: {
-		'appc.mysql': {
-			connectionPooling: true,
-			connectionLimit: 10,
-
-			database: 'test',
-			user: 'root',
-			password: '',
-			host: 'localhost',
-			port: 3306
+		'com.connector.owa': {
+            modelAutogen: true,
+            openWeatherApiUrl : 'http://api.openweathermap.org/data/2.5/',
+            town : "Sofia",
+			numberOfDays: 5,
+			unitsFOrmat: "metric",
+			dataFormat: "json",
+            apikey : "dc98411c082bfb665d92c4e337b1b562",
 		}
 	},
 
